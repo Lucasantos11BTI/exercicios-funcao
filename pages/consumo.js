2448de3160b5b7794ba9882593d726dfef84c2e4
Lucasantos11BTI Lucas Nunes <lucasnbds8@gmail.com>
2025-05-12 11:51:50 +0000

    resolução do exercício 1014- consumo

---
+++ pages/consumo.js
@@ -0,0 +1,9 @@
+const consumoMedio = (distancia, combustivel) => (distancia / combustivel).toFixed(3);
+
+let resultado1 = consumoMedio(500, 35.0);
+let resultado2 = consumoMedio(2254, 124.4);
+let resultado3 = consumoMedio(4554, 464.6);
+
+console.log(`${resultado1} km/l`);
+console.log(`${resultado2} km/l`);
+console.log(`${resultado3} km/l`);
